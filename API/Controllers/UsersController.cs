852957a28d821b6cb0bb3ced66cbc62a98ddf7f7
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-03-06 12:39:13 -0300

    Metodos assincronos

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,16 +19,16 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetAllThePeople()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAllThePeople()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
 
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUserById(int? id)
+        public async Task<ActionResult<AppUser>> GetUserById(int? id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
