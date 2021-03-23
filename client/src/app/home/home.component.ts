f29e309715c66b70fb9613c4159dc017ae2cbd08
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-03-23 11:02:46 -0300

    Comunicacao entre home e registro

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,11 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit
 {
   registerMode = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void
   {
+    this.getUsers();
   }
 
   registerToggle()
@@ -20,4 +23,13 @@ export class HomeComponent implements OnInit
     this.registerMode = !this.registerMode;
   }
 
+  getUsers() 
+  {
+    this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
+  }
+
+  cancelRegisterMode(event: boolean)
+  {
+    this.registerMode = event;
+  }
 }
