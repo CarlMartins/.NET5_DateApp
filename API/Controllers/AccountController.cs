3fbbdc60db7ca3f2be0163abaa39a290cd20cc5e
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-09-12 15:29:19 -0300

    Created login method with password and username validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -39,6 +39,26 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(loginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(u => u.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computerHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computerHash.Length; i++)
+            {
+                if (computerHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(u => u.UserName == username.ToLower());
