fd232f7082044530c6cdee629e412add732a383c
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-09-18 14:16:03 -0300

    Created navbar component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,25 +4,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getUsers();
   }
 
   getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(res => {
-      this.users = res;
-    }, err => {
-      console.log(err)
-    });
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (res) => {
+        this.users = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
