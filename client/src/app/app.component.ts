dc23c73b6ccdd5c499c08998c7f38daa4b73c256
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-09-19 15:29:48 -0300

    Implemented persistent login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { AccountService } from './_services/account.service';
+import { IUser } from './_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,10 +12,19 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
     this.getUsers();
+    this, this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: IUser = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
 
   getUsers() {
