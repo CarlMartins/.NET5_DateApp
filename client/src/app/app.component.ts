634b7aa6e34d2ca5f08525cf20c6d3d56e50dddb
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-09-21 18:06:02 -0300

    Parent to tchild communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,29 +12,14 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this, this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: IUser = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (res) => {
-        this.users = res;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
   }
 }
