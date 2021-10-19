843ba5a64ec742bee36d296438826cc1927e8eb1
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-10-19 19:01:11 -0300

    Styling the cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,10 @@
 import { IMember } from './../../_models/member';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: IMember;
