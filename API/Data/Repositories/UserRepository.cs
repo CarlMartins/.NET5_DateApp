b03bf081da4a4fbaab5c49a54e7c727b135c0a95
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-12-02 20:20:00 +0100

    Added filtering to the API

--- API/Data/Repositories/UserRepository.cs
+++ API/Data/Repositories/UserRepository.cs
@@ -54,11 +54,16 @@ namespace API.Data.Repositories
 
         public async Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams)
         {
-            var query = _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .AsNoTracking();
+            var query = _context.Users.AsQueryable();
 
-            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(
+                _mapper.ConfigurationProvider).AsNoTracking(), 
+                userParams.PageNumber, 
+                userParams.PageSize
+                );
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
