2af8575e7231747a513c0c67cb55dee09f98649a
CarlMartins <lemos_oliveiraa@hotmail.com>
2021-12-31 15:29:00 +0100

    Implemented message sender functionality on client

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IMessage } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
+import { MembersService } from '../../_services/members.service';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,10 +11,22 @@ import { MessageService } from '../../_services/message.service';
   styleUrls: ['./member-messages.component.css'],
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   @Input() messages: IMessage[];
+  @Input() username: string;
+  messageContent: string;
 
-  constructor() { }
+  constructor
+  (private messageService: MessageService) {}
 
-  ngOnInit(): void {
+  ngOnInit()
+    :
+    void {}
+
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
+      this.messages.push(message);
+      this.messageForm.reset();
+    });
   }
 }
