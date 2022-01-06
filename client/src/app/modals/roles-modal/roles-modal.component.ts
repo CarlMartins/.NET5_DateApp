067ef8a0991bcdca2298a7eb8f05d9b753ea503d
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-06 18:51:09 -0300

    Editing roles on client

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { IUser } from '../../_models/user';
 
 
 @Component({
@@ -8,13 +9,17 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./roles-modal.component.css'],
 })
 export class RolesModalComponent implements OnInit {
-  title: string;
-  list: any[] = [];
-  closeBtnName: string;
+  @Input() updateSelectedRoles = new EventEmitter();
+  user: IUser;
+  roles: any[];
 
   constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
   }
 
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
+  }
 }
