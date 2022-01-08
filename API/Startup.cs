e7fa51845c809b4c469ce2760d40eafea891f3bd
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-08 18:40:24 -0300

    Sendind messages via the hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -64,7 +64,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
-                endpoints.MapHub<PresenceHub>("hubs/message");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -6,6 +6,7 @@ import { IMessage } from '../_models/message';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { IUser } from '../_models/user';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 
 @Injectable({
@@ -35,6 +36,12 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
     });
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread$.pipe(take(1)).subscribe(messages => {
+        this.messageThreadSource.next([...messages, message]);
+      });
+    });
   }
 
   stopHubConnection() {
@@ -53,8 +60,9 @@ export class MessageService {
     return this.http.get<IMessage[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<IMessage>(this.baseUrl + 'messages', { recipientUsername: username, content });
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection.invoke('SendMessage', { recipientUsername: username, content })
+      .catch((error) => console.log(error));
   }
 
   deleteMessage(id: number) {
