b8ecd416cac646b3b49415ffd3a06f2ef4261ec0
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-12 18:38:16 -0300

    Creating an angular production build

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -3,11 +3,12 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
 import { delay, finalize } from 'rxjs/operators';
+
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -17,10 +18,9 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => {
         this.busyService.idle();
-      })
+      }),
     );
   }
 }
